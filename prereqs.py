afd9bf1232ed9b195841b57c4f628f52dd797803
paavansaireddy <79843129+paavansaireddy@users.noreply.github.com>
2024-04-28 13:55:33 -0500

    Update prereqs.py

--- prereqs.py
+++ prereqs.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import subprocess
+import shutil
 
 # Define the directory where the repo should be cloned
 directory = '/content/gaussian-splatting'
@@ -24,11 +25,32 @@ if os.path.exists("train.py"):
     os.remove("train.py")
     print("Existing train.py removed.")
 
-# Download the new train.py from the given URL
-response = requests.get(url)
-if response.status_code == 200:
-    with open("train.py", "wb") as f:
-        f.write(response.content)
-    print("train.py has been updated.")
-else:
-    print(f"Failed to download the file. Status code: {response.status_code}")
+source_file = "/content/GussianSplatting/train.py"
+destination_file = "/content/gaussian-splatting"
+
+# Copy the file
+try:
+    shutil.copy(source_file, destination_file)
+    print(f"File copied successfully from {source_file} to {destination_file}")
+except IOError as e:
+    # Handle the error, e.g., source or destination doesn't exist
+    print(f"Failed to copy file: {e}")
+except Exception as e:
+    # Handle other exceptions, such as permission issues
+    print(f"An error occurred: {e}")
+
+source_path1 = "/content/GussianSplatting/playroom"
+destination_path1 = "/content/gaussian-splatting"
+
+# Copy the file
+try:
+    shutil.move(source_path1, destination_path1)
+    print(f"File moved successfully from {source_path1} to {destination_path1}")
+except IOError as e:
+    # Handle the error, e.g., source or destination doesn't exist
+    print(f"Failed to move file: {e}")
+except Exception as e:
+    # Handle other exceptions, such as permission issues
+    print(f"An error occurred: {e}")
+
+
